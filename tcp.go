e34a2d8f4765f426895bd6f2350b3fa04cfd4537
BuMaRen <hj.961110@qq.com>
2026-01-09 17:36:25 +0000

    transparent初步完成ip解析

--- tcp.go
+++ tcp.go
@@ -4,33 +4,45 @@ import (
 	"fmt"
 	"io"
 	"net"
-	"syscall"
+	"time"
 
 	"golang.org/x/sys/unix"
 )
+
+const idleTimeout = 10 * time.Second
 
 func TCPServer() {
 	listener, _ := net.Listen("tcp", ":15001")
 	for {
 		in, _ := listener.Accept()
 		go func() {
+			defer in.Close()
+			_ = in.SetDeadline(time.Now().Add(idleTimeout))
 			sc, _ := in.(*net.TCPConn).SyscallConn()
-			originalDst := &net.TCPAddr{}
+			originalDst := &net.TCPAddr{Port: 16001}
+			var dstErr error
 			sc.Control(func(fd uintptr) {
-				ipBytes, _ := syscall.GetsockoptInet4Addr(int(fd), syscall.IPPROTO_TCP, unix.SO_ORIGINAL_DST)
-				originalDst := net.TCPAddr{
-					IP:   net.IPv4(ipBytes[0], ipBytes[1], ipBytes[2], ipBytes[3]),
-					Port: 16001,
+				// SO_ORIGINAL_DST returns a sockaddr_in in IPv6Mreq.Multiaddr
+				addr, err := unix.GetsockoptIPv6Mreq(int(fd), unix.IPPROTO_IP, unix.SO_ORIGINAL_DST)
+				if err != nil {
+					dstErr = fmt.Errorf("getsockopt SO_ORIGINAL_DST: %w", err)
+					return
 				}
-				originalDst.IP = net.IPv4(ipBytes[0], ipBytes[1], ipBytes[2], ipBytes[3])
-				originalDst.Port = 16001
+				originalDst.IP = net.IPv4(addr.Multiaddr[4], addr.Multiaddr[5], addr.Multiaddr[6], addr.Multiaddr[7])
 			})
-			out, err := net.Dial("tcp", originalDst.String())
-			if err != nil {
-				defer in.Close()
-				fmt.Printf("can not connect to original dst: %v\n", err)
+			if dstErr != nil {
+				fmt.Printf("failed to obtain original dst: %v\n", dstErr)
+				in.Close()
 				return
 			}
+			out, err := net.DialTimeout("tcp", originalDst.String(), 5*time.Second)
+			// out, err := net.Dial("tcp", originalDst.String())
+			if err != nil {
+				fmt.Printf("can not connect to original dst: %v, err: %v\n", originalDst.String(), err)
+				return
+			}
+			defer out.Close()
+			_ = out.SetDeadline(time.Now().Add(idleTimeout))
 			go io.Copy(in, out)
 			go io.Copy(out, in)
 		}()
