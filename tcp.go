f0f4e99aaf2401c29c7c54dbc7f6466e999ad9b4
BuMaRen <hj.961110@qq.com>
2026-01-11 06:34:54 +0000

    debug log

--- tcp.go
+++ tcp.go
@@ -17,6 +17,7 @@ func TCPServer() {
 	for {
 		in, _ := listener.Accept()
 		go func() {
+			fmt.Println("[Debug] new connection coming")
 			defer in.Close()
 			_ = in.SetDeadline(time.Now().Add(idleTimeout))
 			sc, _ := in.(*net.TCPConn).SyscallConn()
@@ -37,6 +38,7 @@ func TCPServer() {
 				in.Close()
 				return
 			}
+			fmt.Printf("[Debug] Dialing %v:%v\n", originalDst.IP.String(), originalDst.Port)
 			out, err := net.DialTimeout("tcp", originalDst.String(), 5*time.Second)
 			if err != nil {
 				fmt.Printf("can not connect to original dst: %v, err: %v\n", originalDst.String(), err)
@@ -54,6 +56,7 @@ func TCPServer() {
 				if err != nil {
 					fmt.Printf("close write to in error: %v\n", err)
 				}
+				fmt.Printf("finish transfer from out to in\n")
 			}()
 			go func() {
 				defer wg.Done()
@@ -62,8 +65,10 @@ func TCPServer() {
 				if err != nil {
 					fmt.Printf("close write to out error: %v\n", err)
 				}
+				fmt.Printf("finish transfer from in to out\n")
 			}()
 			wg.Wait()
+			fmt.Printf("connection from %v to %v closed\n", in.RemoteAddr().String(), originalDst.String())
 		}()
 	}
 }
